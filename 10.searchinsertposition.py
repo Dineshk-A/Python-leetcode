544c2df10e2f468db1eea77530bbfab99bc6b56b
Kaidoo <160697052+Dineshk-A@users.noreply.github.com>
2024-12-29 22:49:29 +0530

    Search insert positon

---
+++ 10.searchinsertposition.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchInsert(nums: list[int], target: int) -> int:
+        l,r = 0,len(nums) - 1
+
+        while l <= r:
+            mid = (l+r) // 2 
+
+            if target == nums[mid]: #luckily what if we get value at mid
+                return mid
+            if target > nums[mid]:
+                l = mid + 1
+            else :
+                r = mid - 1
+        return l
+    
+nums = [1,2,3,4,6]
+target = 5
+result = Solution.searchInsert(nums,target)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+# we can 0(n) , we can do better since its sorted array , logn 
+# using standard algorithm , Binary search . not gonna search nin every index of valye 
+# left pointer , right pointer , middle pointer /
+# l+r/2 = middle pointer .-> o(logn) > o(n).
--- 8.removeElements.py
+++ 8.removeElements.py
@@ -12,3 +12,7 @@ nums = [0,1,2,2,3,0,4,2]
 val = 2
 result = Solution.remove(nums,val)
 print(result)
+
+# overwriting elements that are not equal to val to the front of the list.
+# New length: The final length of the list, without the elements equal to val, is k.
+# This technique is often used to modify lists in place, removing elements that match a certain value, and returning the new length of the list.
